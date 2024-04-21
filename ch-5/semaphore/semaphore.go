1d9efbd54c89021137dd3356c5f6ae7e44a83f6a
Nivekithan S <nivekithan363@gmail.com>
2024-04-21 19:57:58 +0530

    Semaphore

---
+++ ch-5/semaphore/semaphore.go
@@ -0,0 +1,37 @@
+package semaphore
+
+import (
+	"sync"
+)
+
+type Semaphore struct {
+	permits int
+	cond    *sync.Cond
+}
+
+func NewSemaphore(initalPermits int) *Semaphore {
+	return &Semaphore{
+		permits: initalPermits,
+		cond:    sync.NewCond(&sync.Mutex{}),
+	}
+}
+
+func (s *Semaphore) Release(permit int) {
+	s.cond.L.Lock()
+
+	defer s.cond.L.Unlock()
+
+	s.permits += permit
+	s.cond.Signal()
+}
+
+func (s *Semaphore) Acquire(permit int) {
+	s.cond.L.Lock()
+	defer s.cond.L.Unlock()
+
+	for s.permits <= permit-1 {
+		s.cond.Wait()
+	}
+
+	s.permits -= permit
+}
